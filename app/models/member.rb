fdce6549dba7ee2e281221ea97730bfff9bb4562
Mizuki <ne191217@senshu-u.jp>
2020-10-10 08:48:48 +0000

    2020/10/08 問題1-4改

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,19 @@
 class Member < ApplicationRecord
+    class << self
+        def search(query,man,lady)
+          rel = order("number")
+          if query.present?
+            rel = rel.where("name LIKE ? OR full_name LIKE ?",
+              "%#{query}%", "%#{query}%")
+          end
+          if(lady=="1")
+            if(man!="1")
+              rel=rel.where(sex:2)
+            end
+          elsif(man=="1")
+            rel=rel.where(sex:1)
+          end
+          rel
+        end
+      end
 end
