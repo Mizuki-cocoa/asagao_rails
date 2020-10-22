5cf2735deffac5c9f90734b156bc4ae1f757fa2c
Mizuki <ne191217@senshu-u.jp>
2020-10-22 08:24:49 +0000

    第５回授業内課題

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :update_expiration_time
+
     private def current_member
-        Member.find_by(id: session[:member_id]) if session[:member_id]
+        Member.find_by(id: cookies.signed[:member_id]) if cookies.signed[:member_id]
     end
+
     helper_method :current_member
 
     class LoginRequired < StandardError; end
@@ -15,6 +18,13 @@ class ApplicationController < ActionController::Base
     
     rescue_from LoginRequired, with: :rescue_login_required
     rescue_from Forbidden, with: :rescue_forbidden
+
+    private def update_expiration_time
+        member=current_member
+        if member#値があれば
+            cookies.signed[:member_id]={value: member.id , expires: 10.seconds.from_now}
+        end
+    end
 
     private def login_required
         raise LoginRequired unless current_member
