5840ec032b6c8eac1cc398effba52bb816b811f5
Mizuki <ne191217@senshu-u.jp>
2020-12-17 08:06:13 +0000

    Heroku用の設定4

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,11 +1,12 @@
 class ErrorsController < ActionController::Base
-    layout "error"
+  layout "error"
   
-    def show
-      ex= request.env["action_dispatch.exception"]
-      when ActionController::RoutingError
-        render "not_found", status: 404, formats: [:html]
-      else
-        render "internal_server_error", status: 500, formats: [:html]
-      end
+  def show
+    ex= request.env["action_dispatch.exception"]
+    when ActionController::RoutingError
+      render "not_found", status: 404, formats: [:html]
+    else
+      render "internal_server_error", status: 500, formats: [:html]
     end
+  end
+end
